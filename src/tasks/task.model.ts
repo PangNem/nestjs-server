a8d8617be5bd76db46122afa350091e76094e592
pangnem <sprnd645@gmail.com>
2020-08-21 01:55:18 +0900

    test: 테스트 추가

--- src/tasks/task.model.ts
+++ src/tasks/task.model.ts
@@ -10,3 +10,24 @@ export enum TaskStatus {
   IN_PROGRESS = 'IN_PROGRESS',
   DONE = 'DONE',
 }
+
+export const testTasks = [
+  {
+    id: '1',
+    title: '1',
+    description: '1',
+    status: TaskStatus.OPEN,
+  },
+  {
+    id: '2',
+    title: '2',
+    description: '2',
+    status: TaskStatus.IN_PROGRESS,
+  },
+  {
+    id: '3',
+    title: '3',
+    description: '3',
+    status: TaskStatus.DONE,
+  },
+];
--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,8 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { testTasks, TaskStatus } from './task.model';
 
 describe('TasksService', () => {
   let service: TasksService;
+
+  const deepCopiedTestTasks = Array.from(testTasks);
+  const newTaskInfo = { title: '4', description: '4' };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,7 +16,41 @@ describe('TasksService', () => {
     service = module.get<TasksService>(TasksService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getAllTasks', () => {
+    it('get all tasks from an temporary array', () => {
+      expect(service.getAllTasks()).toEqual(testTasks);
+    });
+  });
+
+  describe('getTaskById', () => {
+    it('get a task by id', () => {
+      expect(service.getTaskById('1')).toEqual(testTasks[0]);
+    });
+  });
+
+  describe('createTask', () => {
+    it('create a new task and return its task', () => {
+      expect(service.createTask(newTaskInfo)).toEqual({
+        ...newTaskInfo,
+        id: '4',
+        status: TaskStatus.OPEN,
+      });
+    });
+  });
+
+  describe('updateTaskStatus', () => {
+    it('update a created task', () => {
+      expect(service.updateTaskStatus('4', TaskStatus.IN_PROGRESS)).toEqual({
+        ...newTaskInfo,
+        id: '4',
+        status: TaskStatus.IN_PROGRESS,
+      });
+    });
+  });
+
+  describe('deleteTask', () => {
+    it('delete a task by id', () => {
+      expect(service.deleteTask('4')).toEqual(deepCopiedTestTasks);
+    });
   });
 });
