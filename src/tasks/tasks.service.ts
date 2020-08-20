1f405aa06dc4c55d87716eac3fe5ea834ba76b70
pangnem <sprnd645@gmail.com>
2020-08-21 01:56:05 +0900

    implement: TasksService 구현

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { Task, testTasks, TaskStatus } from './task.model';
+import { CreateTaskDTO } from './dto/create-task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = testTasks;
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+  getTaskById(id: string): Task {
+    return this.tasks.filter(task => task.id === id)[0];
+  }
+
+  createTask(createTaskDTO: CreateTaskDTO): Task {
+    const { title, description } = createTaskDTO;
+
+    const task: Task = {
+      id: (this.tasks.length + 1).toString(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+
+  updateTaskStatus(id: string, status: string): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+
+    return task;
+  }
+
+  deleteTask(id: string): Task[] {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+    return this.tasks;
+  }
+}
